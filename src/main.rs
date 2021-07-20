0b4dc7f44c7bd6a6b0c721ac11c59d493a6435f5
Amiel <amielnurja@gmail.com>
2021-07-20 15:54:04 -0400

    Implemented checking for 4 in a row in all directions, needs testing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
 use std::io;
 use ansi_term;
 
 fn main() {
     let mut board = Board::new(Colour::Red);
-
+    board.print_grid();
     while board.running {
-        let mut col: u8;
-
-        board.print_grid();
-
+        let mut col: u8; 
         loop {
             let mut input = String::new();
             io::stdin()
@@ -25,11 +24,21 @@ fn main() {
         }
         
         board.play_turn(col.into());
-        
+        board.print_grid();
+
+        let win_state: Option<Colour> = board.check_win();
+        if win_state != None {
+            match win_state.unwrap() {
+                Colour::Red => println!("Red wins!"),
+                Colour::Yellow => println!("Yellow wins!"),
+            }
+            board.running = false;
+        }
+
     }  
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum Colour {
     Red,
     Yellow,
@@ -37,7 +46,7 @@ pub enum Colour {
 
 #[derive(Copy, Clone)]
 pub struct Board {
-    pub grid: [Option<Colour>; 42],
+    pub grid: [Option<Colour>; 42], // 7x6 grid
     pub turn: Colour,
     pub running: bool
 }
@@ -45,13 +54,14 @@ pub struct Board {
 impl Board {
     pub fn new(start_colour: Colour) -> Self {
         Board {
-            grid: [None; 42], // 7x6 grid
+            grid: [None; 42], 
             turn: start_colour,
             running: true
         }
     }
 
     pub fn is_valid_move(&self, col: usize) -> bool {
+        // Valid move if specified column in top row is empty
         if col >= 7 { return false }
         match self.grid[col] { Some(_) => false, None => true }
     }
@@ -68,8 +78,8 @@ impl Board {
                 if ind % 7 == 0 { print!("│") } // Left border
 
                 match self.grid[ind] {
-                    Some(color) => {
-                        match color {
+                    Some(c) => {
+                        match c {
                             Colour::Red => {
                                 print!("{}", ansi_term::Colour::Red.paint("O"));
                             },
@@ -90,6 +100,7 @@ impl Board {
     }
 
     pub fn play_turn(&mut self, col: usize) {
+        // "Drops" the current player's piece into the grid 
         let mut ind: usize = col;
         loop {
             match self.grid[ind + 7] {
@@ -106,6 +117,70 @@ impl Board {
             Colour::Red => self.turn = Colour::Yellow,
             Colour::Yellow => self.turn = Colour::Red,
         }
+
+    }
+
+    pub fn check_win(&self) -> Option<Colour> {
+        // Checks entire board for 4 in a row
+        
+        let mut w = false;
+        for i in 0..42 {
+            if self.grid[i] == None { continue; }
+            let c: Colour = self.grid[i].unwrap();
+
+            // Horizontal check (left to right)
+            if i % 7 < 4 { // Ignore 3 rightmost columns
+                for delta in 1..4 {
+                    if self.grid[i+delta] != None { // Avoid unwrapping None
+                        if self.grid[i+delta].unwrap() != c { break; }
+                        if delta == 3 { w = true; }
+                    } else { break; } // Adjacent piece is None, no 4 in a row
+                }
+
+                if w { return Some(c); }
+            }
+
+
+            // Vertical check (this check and the next two are descending)
+            if i < 20 { // Ignore bottom 3 rows
+                for delta in 1..4 {
+                    if self.grid[i+7*delta] != None {
+                        if self.grid[i+7*delta].unwrap() != c { break; }
+                        if delta == 3 { w = true; }
+                    } else { break; } 
+                }
+
+                if w { return Some(c); }
+            }
+
+
+            // Right diagonal check
+            if i < 17 { // Last possible index for diag to start
+                for delta in 1..4 {
+                    if self.grid[i+8*delta] != None {
+                        if (i+8*delta) % 7 == 0 { break; } // Avoid wraparound
+                        if self.grid[i+8*delta].unwrap() != c { break; }
+                        if delta == 3 { w = true; }
+                    } else { break; }
+                }
+                if w { return Some(c); }
+            }
+
+
+            // Left diagonal check
+            if i < 20 { // Last possible index for diag to start
+                for delta in 1..4 {
+                    if self.grid[i+6*delta] != None {
+                        if (i+6*delta) % 7 == 6 { break; } // Avoid wraparound
+                        if self.grid[i+6*delta].unwrap() != c { break; }
+                        if delta == 3 { w = true; }
+                    } else { break; }
+                }
+            }
+            if w { return Some(c); }
+        }
+
+        None
     }
 
 }
